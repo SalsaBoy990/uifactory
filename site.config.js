28843822318745b034d7d813fec2053ba9caefd8
Andras Gulacsi <guland@protonmail.com>
2019-03-21 03:28:55 +0100

    test

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
     quote: 'UI/UX designer-nek tanulok és munkát keresek UI designer pozícióba. Az Udemy kurzusaiból tanulok (oktatóim: Muhammad Ahsan Pervaiz, Joe Natoli, Krisztina Szerovay), továbbá teljesítettem az UXstudio nyolc hetes Digital Product Design tanfolyamát is.',
     description: 'UI/UX designer-nek tanulok és munkát keresek UI designer pozícióba.',
     currentYear: new Date().getFullYear(),
-    google_analytics: 'YOUR-ID-COMES-HERE',
+    google_analytics: 'Missing',
     linkedin: 'https://www.linkedin.com/in/andr%C3%A1s-gul%C3%A1csi-548b5a176/',
     email: 'mailto:gulandras90@gmail.com',
     cv: 'https://www.docdroid.net/QnUORxI/gulacsi-andras-oneletrajz-2018.pdf'
